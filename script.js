dd1f9a08575a8026775118486dc2dabf0bd4e231
bishwa <bishwa@localhost.localdomain>
2023-07-08 14:54:52 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
